690a5b3e2991f766f8d69537453299be9f104652
patricknogaj <patricknogaj@gmail.com>
2020-03-14 16:44:45 -0400

    Output/storing values into variables
    Output implemented.
    Stored argument values into a variable rather than calling the argument itself.

--- main.py
+++ main.py
@@ -7,15 +7,27 @@
 import sys
 
 # Creating an empty dictionary titled 'firstWord'
+# Storing the value of first argument into a string
 firstWord = {}
+firstArgument = str(sys.argv[1])
 
 # This is storing the value of each letter in firstWord (first argument)
 # to a value, by default it will be 1 unless it already exists
 # which will then update the value by 1.
-for letter in str(sys.argv[1]):
+for letter in firstArgument:
     if(firstWord.get(letter)):
         firstWord[letter] = firstWord.get(letter) + 1
     else:
         firstWord[letter] = 1
 
+# Storing the second argument into a string
+secondArgument = str(sys.argv[2])
+
+# If the value of the dictionary equals the length of argument 2
+# return true
+# otherwise, they are not the same thus it is false.
+if(len(firstWord) == len(secondArgument)):
+   print 'true'
+else:
+    print 'false'
 
